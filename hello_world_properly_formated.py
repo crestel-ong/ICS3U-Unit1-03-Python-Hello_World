2e3b6d0f7cce1f76cfe7ac444adb16f32ba96a76
crestel-ong <crestel.ong@mths.ca>
2021-09-13 21:47:10 +0000

    in proper repo

---
+++ hello_world_properly_formated.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Crestel Ong
+# Created on: Sept 2020
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
